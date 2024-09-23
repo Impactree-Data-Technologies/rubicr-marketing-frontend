bfb345197bbd715f7afbbf72e0cbad11a6338161
AbrahamCatchirayar <abraham@impactree.ai>
2024-09-23 18:24:55 +0530

    six step updated

--- src/app/Components/sixstep.tsx
+++ src/app/Components/sixstep.tsx
@@ -20,7 +20,7 @@ interface StepProps extends StepData {
 const Step: React.FC<StepProps> = ({ number, title, description, onHover, onLeave, isHovered }) => (
   <div className="flex flex-col items-center relative group">
     <div
-      className={`flex items-center justify-center w-12 h-12 rounded-full mb-2 relative ${isHovered ? 'bg-green-500' : 'bg-gray-300'}`}
+      className={`flex items-center justify-center w-12 h-12 rounded-full mb-2 relative ${isHovered ? 'bg-green-500' : 'bg-gray-950'}`}
       onMouseEnter={onHover}
       onMouseLeave={onLeave}
     >
@@ -32,7 +32,7 @@ const Step: React.FC<StepProps> = ({ number, title, description, onHover, onLeav
     <div className={`absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 p-4 bg-white text-black text-sm rounded shadow-md z-10 w-72 opacity-0 group-hover:opacity-100 transition-opacity duration-200 pointer-events-none ${number === 1 ? 'md:left-0 md:translate-x-0' : number === 6 ? 'md:left-auto md:right-0 md:translate-x-0' : ''}`}>
       <p>{description}</p>
     </div>
-    <h3 className="text-sm font-medium text-center mt-2">{title}</h3>
+    <h3 className="text-sm font-bold text-center mt-2">{title}</h3>
   </div>
 );
 
@@ -81,12 +81,12 @@ const SixStep: React.FC = () => {
               {index < sixStepData.step_card.length - 1 && (
                 <>
                   <div className="md:hidden flex flex-col justify-center items-center h-16 w-full">
-                    <div className="h-12 w-1 bg-gray-400"></div>
-                    <div className="w-0 h-0 border-l-8 border-r-8 border-t-8 border-l-transparent border-r-transparent border-t-gray-400"></div>
+                    <div className="h-12 w-1 bg-gray-950"></div>
+                    <div className="w-0 h-0 border-l-8 border-r-8 border-t-8 border-l-transparent border-r-transparent border-t-gray-950"></div>
                   </div>
                   <div className="hidden md:flex flex-1 items-center justify-center">
-                    <div className="w-16 h-1 bg-gray-400 -translate-y-6"></div>
-                    <div className="w-0 h-0 border-t-8 border-t-transparent border-b-8 border-b-transparent border-l-8 border-l-gray-400 -translate-y-6"></div>
+                    <div className="w-16 h-1 bg-gray-950 -translate-y-6"></div>
+                    <div className="w-0 h-0 border-t-8 border-t-transparent border-b-8 border-b-transparent border-l-8 border-l-gray-950 -translate-y-6"></div>
                   </div>
                 </>
               )}
