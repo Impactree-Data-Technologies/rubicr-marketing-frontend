ab88d4b56ae8d5c15d7d01843e987e5e29e41ee2
swethamain <swetha@impactree.ai>
2025-01-29 16:56:20 +0530

    hotjar script added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,6 +38,22 @@ export default function RootLayout({
         data-chatbotid="7e05cd22-8058-4899-864d-f8ac1e765708"
         data-url="https://guidelite.ai"
       />
+        <Script
+        id="hotjar-tracking"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:5276456,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+          `
+        }}
+      />
       <body>
         <noscript
           dangerouslySetInnerHTML={{
